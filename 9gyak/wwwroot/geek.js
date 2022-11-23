9f828b1fa49c6d3ba668cedea818df086cd5edf6
ilevente22 <levente.illes@stud.uni-corvinus.hu>
2022-11-23 11:18:49 +0100

    Dolgoztam órán, de lehet rossz

---
+++ 9gyak/wwwroot/geek.js
@@ -0,0 +1,19 @@
+﻿var viccek;
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+
+    for (var i = 0; i < viccek.length; i++) {
+        viccf = JSON.parse(viccek)
+        var viccf = new Array()
+    }
+}
+window.onload(letöltés());
